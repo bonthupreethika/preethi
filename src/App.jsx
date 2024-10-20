934dd75bae2a1205be7944616ba1fd909ec53c2f
bonthupreethika <preethikabonthu@gmail.com>
2024-10-20 11:22:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Header from "./Header";
 import Footer from "./Footer";
